67cede37005704f5be672d4c40e940935c745589
zcz1997 <zcz1997@hotmail.com>
2025-05-15 21:49:09 +0900

    feat:Day3 输入练习，限制字符数功能

---
+++ pages/day3.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Day 3</h1>
+        <p>This is the third day of Vue.js</p>
+        <input type="text" v-model="message" placeholder="请输入你的名字" maxlength="20"/>
+        <p v-if="message">你好，{{ message }}</p>
+        <p v-else>你还没有输入你的名字</p>
+        <button @click="message = ''">清空输入框</button>
+        <p>当前输入：{{ message.length }} / 20 字</p>
+    </div>
+</template>
+
+<script setup>
+import { ref,watch } from 'vue'
+//限制message最长10个字
+const message = ref('')
+watch(message, (newVal, oldVal) => {
+        if (newVal.length > 20) {
+            message.value = newVal.slice(0, 20)
+            alert('最多只能输入20个字')
+            }
+    }
+    )
+
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,18 @@
     <p >This is a simple Nuxt.js app with a few basic pages and components.</p>
     <p style=" font-size: 18px; font-family: 'Times New Roman', Times, serif;">Welcome to my Nuxt App!</p>    
     <img src="https://avatars.githubusercontent.com/u/33356927?v=4" alt="logo" style="width: 100px; height: 100px;" />
-    <a href="https://github.com/zcz1997" style="color: aqua;">click to go to my github</a>
+    <a href="https://github.com/zcz1997" style="color: aqua;" target="_blank">click to go to my github</a>
     <p style="text-align: center;">Today is first day i learn Vue.js</p>
     <button style="color: azure;border-radius: 20px; background-color: black; padding: 10px 20px; cursor: pointer;" @click="increment">click me</button>
     <p>you clicked me {{ count }} times</p>
+    <p>ようこそ、以下是每日练习入口：</p>
+
+    <ul>
+      <li><NuxtLink to="/">📗 Day 1 - Nuxt 页面显示</NuxtLink></li>
+      
+      <li><NuxtLink to="/day3">📙 Day 3 - v-model 双向绑定</NuxtLink></li>
+      <!-- 后续可以继续添加 Day 4, 5 等 -->
+    </ul>
   </div>
 </template>
 <script setup>
